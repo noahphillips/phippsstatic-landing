d3654884cc3b23a0d5c98d5f8da818b5c6bd437b
Noah Phillips <noah@jms-nphillips.local>
2017-04-13 00:54:27 -0700

    Updates server directory structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http            = require('http')
 var ecstatic        = require('ecstatic')
 
 http.createServer(
-  ecstatic({ root: __dirname + '/public' })
+  ecstatic({ root: __dirname + '/' })
 ).listen(11001);
 
 console.log('server listening on http://localhost:11001')
